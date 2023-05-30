5db69a18440b495e7f6c63aa33300ef133054199
Frodriks <frodriks@gmail.com>
2023-05-30 12:12:54 +0200

    koood
    update

--- helpers/comments_helper.js
+++ helpers/comments_helper.js
@@ -1,7 +1,9 @@
 import { randFullName, randEmail, randPost } from '@ngneat/falso';
 
-export const  createRandomComment = () => {
+export const  createRandomComment = (userid, postId) => {
     const data = {
+        user_id: userid,
+        post_id: postId,
         name: randFullName({gender: 'female'}),
         email: randEmail({provider: 'jenseneducation', suffix: 'se'}),
         body: randPost().body
--- test/comments.js
+++ test/comments.js
@@ -3,6 +3,7 @@ import { expect } from "chai";
 import dotenv from 'dotenv';
 import { createRandomComment } from "../helpers/comments_helper";
 import { createRandomUser } from "../helpers/user_helper";
+import { createRandomPost } from "../helpers/post_helper";
 
 // Configuration
 dotenv.config();
@@ -12,29 +13,73 @@ const request = supertest('https://gorest.co.in/public/v2/');
 const token = process.env.USER_TOKEN;
 
 // Mocha test cases
-describe('/comments route | Check for comments', () => {
+describe.only('/comments route | Check for comments', () => {
     let userId = null;
     let postId = null;
 
-    it('POST /users | Create user', async () => {
+
+    // Create a user
+    before(async () => {
+        const res = await request
+            .post('users')
+            .set('Authorization', `Bearer ${token}`)
+            .send(createRandomUser);
+
+        userId = res.body.data;
+        console.log(res.body); 
+    });
+/*
+            after(async () => {
+                //this.retries(4);
+                const data = createRandomPost(userId);
+                const res = await request
+                    .post('posts')
+                    .set('Authorization', `Bearer ${token}`)
+                    .send(data);
+
+            postId = res.body.data;
+            console.log(res.body);
+            });    
+    });
+*/
+
+    
+/*
+      //Gjorde en before på detta istället
+    it.only('POST /users | Create user', async () => {
         const data = createRandomUser();
         const res = await request
             .post('users')
             .set('Authorization', `Bearer ${token}`)
             .send(data);
 
-        console.log(res.body.data);
         userId = res.body.data;
-        
+        console.log(res.body);   
+    });
+*/
+
+    it('POST /posts | Create a post', async () => {
+        this.retries(4);
+        const data = createRandomPost(userId);
+        const res = await request
+            .post('posts')
+            .set('Authorization', `Bearer ${token}`)
+            .send(data);
+
+        postId = res.body.data;
+        console.log(res.body);
     });
 
+
+    /* Överflödig kod?
     it('GET /posts', async () => {
         const res = await request.get('posts')
         //console.log(res.body[0])
         postId = res.body[0].id
     });
+    */
 
-    it('GET /comments', async () => {
+    it.skip('GET /comments', async () => {
         const res = await request.get('comments');
         //console.log(res.body);
         expect(res.body).to.not.be.empty;
@@ -47,9 +92,9 @@ describe('/comments route | Check for comments', () => {
     });
     */
 
-    it('POST /comments | Create comment', async () => {
+    it.skip('POST /comments | Create comment', async () => {
         
-        const data = createRandomComment();
+        const data = createRandomComment(postId);
         data.id = postId;
         //console.log(data);
         const res = await request
