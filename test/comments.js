4ce4252c46858f3382e79da42eda91f6ef4b936e
Frodriks <frodriks@gmail.com>
2023-06-01 12:02:26 +0200

    DONE!

--- test/comments.js
+++ test/comments.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 
 // Mocha test cases
-describe.only('/comments route | Check for comments', () => {
+describe('/comments route | Check for comments', () => {
     /* Setup */
     const request = supertest('https://gorest.co.in/public/v2/');
     const token = process.env.USER_TOKEN;
@@ -28,9 +28,6 @@ describe.only('/comments route | Check for comments', () => {
             .send(createRandomUser());
 
         userId = res.body.id;
-        //console.log(res.body.id);
-        //console.log(res.body);
-        // Ska jag ha med expect på helpers?
 
         // Create a post
         const data = createRandomPost(userId);
@@ -40,32 +37,26 @@ describe.only('/comments route | Check for comments', () => {
             .send(data);
 
         postId = res.body.id;
-        //console.log(res.body);
-        // Ska jag ha med expect på helpers?
     });
 
-    it('GET /comments', async () => {
-        const res = await request.get('comments');
+    it('GET /comments | Get comments', async () => {
+        const res = await request
+        .get('comments');
         expect(res.body).to.not.be.empty;
-        //console.log(res.body);
     });
 
     it('POST /comments | Create comment', async () => { 
         const data = createRandomComment(userId, postId);
-        //data.id = postId;
-        //console.log(data);
         const res = await request
             .post('comments')
             .set('Authorization', `Bearer ${token}`)
             .send(data);
-
-            
+  
         commentId = res.body.id;
         expect(res.body.email).to.contain('jenseneducation');
-        //console.log(res.body.id);
     });
 
-    it ('POST /comments | Create comment (Negative)', async () => {
+    it('POST /comments | Create comment (Negative)', async () => {
         const data = {};
         const res = await request
             .post('comments')
@@ -73,21 +64,39 @@ describe.only('/comments route | Check for comments', () => {
             .send(data);
 
         expect(res.status).to.eq(422);
-        //console.log(res.status);
     });
 
-    it ('PUT /comments:id | Change comment', async () => {
+    it('PUT /comments:id | Change comment', async () => {
         const data = {
             name: 'Test Testsson',
             email: 'Test.Testsson@jenseneducation.se',
             body: 'Lets change this comment to something else!'
         };
 
-        const res = await request.put(`comments/${commentId}`)
+        const res = await request
+            .put(`comments/${commentId}`)
             .set('Authorization', `Bearer ${token}`)
             .send(data);
 
-        //console.log(res.body);
+        expect(res.body.name).to.eq('Test Testsson');
+        expect(res.body.email).to.eq('Test.Testsson@jenseneducation.se')
+        expect(res.body.body).to.eq('Lets change this comment to something else!')
+    });
+
+    it('DELETE /comments:id | Delete comment', async () => {
+        const res = await request
+            .delete(`comments/${commentId}`)
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(res.status).to.eq(204)
+    });
+
+    it('DELETE /comments:id | Delete comment (Negative)', async () => {
+        const res = await request
+            .delete(`comments${commentId}`)
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(res.status).to.equal(404)
     });
 
     /* Cleanup */
@@ -95,7 +104,6 @@ describe.only('/comments route | Check for comments', () => {
         const res = await request
             .delete(`users/${userId}`)
             .set('Authorization', `Bearer ${token}`)
-        //console.log(res.body);
     });
     
 });
